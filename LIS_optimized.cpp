bd14e425453a207dc5b3270bcde251406eb3a054
nazeeb1432 <nazeeb.32.mvp@gmail.com>
2023-08-30 16:03:12 +0600

    incomplete

--- LIS_optimized.cpp
+++ LIS_optimized.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define max 100
+int pos;
 
 int L[max],L_prime[max],prv[max];
 
@@ -12,51 +13,61 @@ void LIS_Fast_Dp(int a[],int n){
     int index=0;
 
     for(int i=1;i<=n;i++){
-        if(L[index]<a[i]){
+        if(L[index]<=a[i]){
             L[++index]=a[i];
             L_prime[index]=i;
             prv[i]=L_prime[index-1];
         }
         else{
-
+            int s=0,h=index;
+            while(s<h){
+                int mid=(s+h)/2;
+                if(L[mid]<a[i]) s=mid+1;
+                else h=mid;
+            }
+            L[s]=a[i];
+            L_prime[s]=i;
+            prv[i]=L_prime[s-1];
+                                                                                                                                       
         }
     }
+    pos=index;
+    
 }
 
-int binarySearch(int arr[], int l, int r, int x)
-{
-    while (l <= r) {
-        int m = l + (r - l) / 2;
- 
-       
-        if (arr[m] == x)
-            return m;
- 
-        
-        if (arr[m] < x)
-            l = m + 1;
- 
-        
-        else
-            r = m - 1;
-    }
- 
+void printLIS(int arr[],int i){
+    if(i==-1) return;
+    printLIS(arr,prv[i]);
+    cout<<arr[i]<<" ";
 }
 
 
 int main(){
 
-    int n,arr[max];
+    int n,i,arr[max];
 
     cout<<"How many numbers?:";
     cin>>n;
     cout<<"Enter your sequence:";
+
+    arr[0]=0;
 
     for(int i=1;i<=n;i++){
         cin>>arr[i];
     }
 
     LIS_Fast_Dp(arr,n);
+    cout<<"Length of LIS:"<<pos<<endl;
+
+    cout<<"LIS: ";
+    
+    for(i=0;i<=n;i++){
+        if(arr[i]==L[pos]) break;
+    }
+
+    printLIS(arr,pos-1);
+    cout<<L[pos];
+
 
 
 }
